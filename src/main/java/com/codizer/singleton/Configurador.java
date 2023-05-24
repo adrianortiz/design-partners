dfbb67a64d1fae561261303e85d26d7b0528638f
Adrian Ortiz <adrianortizmartinez@gmail.com>
2023-05-24 00:15:38 -0600

    Singleton

--- src/main/java/com/codizer/singleton/Configurador.java
+++ src/main/java/com/codizer/singleton/Configurador.java
@@ -3,12 +3,19 @@ package com.codizer.singleton;
 public class Configurador {
 
     private String carpeta;
+    private static Configurador configurador;
 
-    public Configurador() {
+    private Configurador(String carpeta) {
+        this.carpeta = carpeta;
     }
 
-    public Configurador(String carpeta) {
-        this.carpeta = carpeta;
+    public static Configurador getInstance() {
+
+        if (configurador == null) {
+            configurador = new Configurador("carpeta-0");
+        }
+
+        return configurador;
     }
 
     public String getCarpeta() {
--- src/main/java/com/codizer/singleton/Principal.java
+++ src/main/java/com/codizer/singleton/Principal.java
@@ -3,6 +3,20 @@ package com.codizer.singleton;
 public class Principal {
 
     public static void main(String[] args) {
-        Configurador c = new Configurador("micarpeta");
+
+        Configurador c1 = Configurador.getInstance();
+        c1.setCarpeta("carpeta-1");
+        System.out.println(c1.getCarpeta());
+
+        Configurador c2 = Configurador.getInstance();
+        c1.setCarpeta("carpeta-2");
+        System.out.println(c2.getCarpeta());
+
+        System.out.println(c1);
+        System.out.println(c2);
+
+        System.out.println(c1.getCarpeta());
+        System.out.println(c2.getCarpeta());
+
     }
 }
